36a839b528d61cdb7d2625c5ff6e9fe06c745d69
Eslam Amin <eslam.amin.610@gmail.com>
2026-01-27 18:41:28 +0200

    Created GenerateDeck function to generate a deck

--- deck.go
+++ deck.go
@@ -6,6 +6,23 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+// genDeck generates a deck of 52 cards with all combinations of suits and values.
+// It returns a slice of strings where each string is a card in the format "value of suit".
+func generateDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 // receiver function to print the deck
 func (d *deck) print() {
 	for _, card := range *d {
